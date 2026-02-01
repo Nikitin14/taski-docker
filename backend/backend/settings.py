f53c097f396b78370e06e8a50616c56fc1124b0d
NikitinM <mixaniksn@yandex.ru>
2026-02-01 18:22:28 +0300

    setup.cfg

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,10 +64,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'postgres'),
-	'USER': os.getenv('POSTGRES_USER', 'postgres'),
-	'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-	'HOST': os.getenv('DB_HOST', ''),
-	'PORT': os.getenv('DB_PORT', '5432')
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', '5432')
     }
 }
 
